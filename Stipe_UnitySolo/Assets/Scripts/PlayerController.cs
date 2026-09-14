2868b92f84b203cd1e6ddd70c29d711d659f7b09
FernieBurnie <stipeisabelle@gmail.com>
2026-09-14 09:57:55 -0700

    movement

--- Stipe_UnitySolo/Assets/Scripts/PlayerController.cs
+++ Stipe_UnitySolo/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpHeight = 10f;
+    public float jumpDetectDistance = 1.1f;
 
     PlayerInput playerInput;
     Rigidbody2D rb;
 
+    Ray2D footJumpRay;
+    Ray2D wallLJumpRay;
+    Ray2D wallRJumpRay;
     Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,19 +23,38 @@ public class PlayerController : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
 
         //Setting up new move Vector
-        moveInput = new Vector2();
+        moveInput = Vector2.zero;
+
+        footJumpRay = new Ray2D(transform.position, -transform.up);
+        wallLJumpRay = new Ray2D(transform.position, -transform.right);
+        wallRJumpRay = new Ray2D(transform.position, transform.right);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 tempMovement = rb.linearVelocity;
+        footJumpRay.origin = transform.position;
+        wallRJumpRay.origin = transform.position;
+        wallLJumpRay.origin = transform.position;
 
-        rb.linearVelocity = moveInput * speed;
+        footJumpRay.direction = -transform.up;
+        wallRJumpRay.direction = transform.right;
+        wallLJumpRay.direction = -transform.right;
+        Vector2 tempMove = rb.linearVelocity;
+
+        tempMove.x = moveInput.x * speed;
+
+        rb.linearVelocity = tempMove;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue <Vector2>();
+        moveInput.x = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump()
+    {
+        if (Physics2D.Raycast(footJumpRay.origin, footJumpRay.direction, jumpDetectDistance) || (Physics2D.Raycast(wallRJumpRay.origin,wallRJumpRay.direction, jumpDetectDistance) || Physics2D.Raycast(wallLJumpRay.origin, wallLJumpRay.direction, jumpDetectDistance)))
+            rb.AddForceY(jumpHeight, ForceMode2D.Impulse);
     }
 }
